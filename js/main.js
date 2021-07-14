36423289de0c570722dc895fe5e716d3c7f867dd
HamsakiBrain <herovuwbdal@gmail.com>
2021-07-15 01:49:17 +0300

    Mobile Version, orderHistory.html

--- js/main.js
+++ js/main.js
@@ -5,4 +5,14 @@ $(document).ready(function() {
             "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Russian.json"
         }
     });
+    let mobile = $("body").width() < 550
+    console.info("Width: ", $("body").width())
+    console.info("Mobile: ",mobile)
+    $(".burger-mobile, .close").on('click', function (){
+        $(".fixed-menu").toggleClass('active')
+    })
+    if (mobile){
+        let aside = $(".aside")
+        $(".fixed-menu").append(aside)
+    }
 } );
