3f4bc7b5fd4acc97e4441f0963cbfb2395a43354
HamsakiBrain <herovuwbdal@gmail.com>
2021-07-14 17:46:51 +0300

    Main (no adaptive)

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#table').DataTable({
+        responsive: !0,
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Russian.json"
+        }
+    });
+} );
